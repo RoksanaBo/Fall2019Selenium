c6e00d6153adadf68d683b98c2711485ce10eba5
RoksanaBo <roksana85@hotmail.com>
2020-03-01 16:51:31 -0600

    created basic navigation class

---
+++ src/test/java/com/automation/tests/day2/GoogleSearchTest.java
@@ -0,0 +1,56 @@
+package com.automation.tests.day2;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GoogleSearchTest {
+
+    public static void main(String[] args) throws Exception{
+
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://google.com");
+        Thread.sleep(2000);
+
+
+        // By.name("q") --> name="q"
+        // name--> one of the selenium locators
+        // there are 8 locators
+        // we use locators to find elements
+        // to choose locators, just use By.locator
+
+        WebElement search = driver.findElement(By.name("q"));
+        // once we found an element, how to enter text?
+        // to enter text,use sendKeys() method
+
+        //how to press Enter after entering the text?
+        // use Key.ENTER --perform keyboard click
+
+        // Keys.ENTER -->perform keyboard click
+        //
+
+        search.sendKeys("Java", Keys.ENTER);
+
+        Thread.sleep(3000);
+
+        //if see <a> element, it calls link
+        // visible text of this link, can be used by selenium to find this element
+
+        WebElement news = driver.findElement(By.linkText("News"));
+        news.click(); // to click on the element
+        Thread.sleep(3000);
+
+        driver.quit();
+
+
+
+
+
+    }
+}
--- src/test/java/com/automation/tests/day2/JumpToNewWindow.java
+++ src/test/java/com/automation/tests/day2/JumpToNewWindow.java
@@ -4,6 +4,8 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.Set;
+
 public class JumpToNewWindow {
 
     public static void main(String[] args)throws Exception {
@@ -21,12 +23,36 @@ public class JumpToNewWindow {
 
         String windowHandle = driver.getWindowHandle();
 
+
+        //    getWindowHandles() returns id's of all currently opened windows
+        // Set - doesn't allow duplicates
+
         System.out.println("window handle: (or window id)" + windowHandle);
 
 
+        Set<String> windowHandles = driver.getWindowHandles(); //
+        System.out.println(windowHandles);
+        System.out.println("before switch : " + driver.getCurrentUrl());
 
+       for(String windowID:windowHandles){
 
+           if(!windowID.equals(windowHandle)){
+               driver.switchTo().window(windowID);
+           }
+       }
+        System.out.println("after switch : " + driver.getCurrentUrl());
+       driver.close();
 
+    }
 
+    public static void switchToWindowBasedOnTitle(String pageTitle, WebDriver driver){
+        Set<String> windows = driver.getWindowHandles();
+        for(String window: windows){
+            driver.switchTo().window(window);
+
+            if(driver.getTitle().equals(pageTitle)){
+               break;
+            }
+        }
     }
 }
